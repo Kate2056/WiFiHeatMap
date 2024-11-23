ba8446c1ed36ca714ffc7f5d9f14a949e385e85c
kbri135 <kbri135@wgu.edu>
2024-11-22 19:28:27 -0600

    Added location permission requests

--- app/src/main/java/com/softwareengineering/wifiheatmap/UI/MainActivity.java
+++ app/src/main/java/com/softwareengineering/wifiheatmap/UI/MainActivity.java
@@ -1,9 +1,13 @@
 package com.softwareengineering.wifiheatmap.UI;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -17,7 +21,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.softwareengineering.wifiheatmap.R;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
-    private MapView mMapView;
+    private static final int PERMISSION_REQUEST_CODE = 200;
     //private static final String MAPVIEW_BUNDLE_KEY = MAPS_API_KEY;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +30,14 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION
+        ) != PackageManager.PERMISSION_GRANTED  && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+        ) != PackageManager.PERMISSION_GRANTED){
+        }else{
+            requestPermissions(new String[]{ Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_CODE);
+
+        }
 
     }
     @Override
